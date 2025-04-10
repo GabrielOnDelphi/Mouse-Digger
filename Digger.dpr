6fd925cf06afa7edeac1b1278cf000551e0278c6
Gabriel Moraru <cubicDesign@gmail.com>
2025-04-10 19:46:17 +0200

    new AppData

--- Digger.dpr
+++ Digger.dpr
@@ -12,5 +12,5 @@ begin
   Application.MainFormOnTaskbar := True;    { If true, a taskbar button represents the application's main form and displays its caption. All child forms will stay on top of the MainForm (bad)! If False, a taskbar button represents the application's (hidden) main window and bears the application's Title. Must be True to use Windows (Vista) Aero effects (ive taskbar thumbnails, Dynamic Windows, Windows Flip, Windows Flip 3D). https://stackoverflow.com/questions/66720721/ }
   Application.CreateForm(TfrmDigger, frmDigger);
   frmDigger.Show;
-  Application.Run;
+  AppData.Run;
 end.
